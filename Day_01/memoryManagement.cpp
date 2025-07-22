ccb62a66962db2151480d38ae0e401a8ca0ad655
Kushal-np <wrckushal@gmail.com>
2025-07-22 16:32:51 +0545

    added comment to even explain myself better

--- Day_01/memoryManagement.cpp
+++ Day_01/memoryManagement.cpp
@@ -8,3 +8,5 @@ int main(){
     cout << *ptr << endl;
     return 0 ; 
 }
+
+// Here , what this new keyword does is , when you assign the value "new int" to the *ptr , it creates a kind of reserved memory for the variable , means, that the ptr is occupying the memory of int but value isn't assigned, which is then done in another line , where we later assigned it the value *ptr = "35";
