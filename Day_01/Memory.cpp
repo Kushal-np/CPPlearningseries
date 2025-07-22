a69dc4565534d25c75e610f7538ab0cb74c9aa3c
Kushal-np <wrckushal@gmail.com>
2025-07-22 16:28:11 +0545

    memory checking

---
+++ Day_01/Memory.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+#include <vector>
+
+int main(){
+    int myInt ; 
+    float myFloat ;
+    double myDouble;
+    char myChar ; 
+
+    cout << sizeof(myInt) << "\n";
+    cout << sizeof(myFloat) << "\n";
+    cout << sizeof(myDouble) << "\n";
+    cout << sizeof(myChar) << "\n";
+    return 0 ;
+}
