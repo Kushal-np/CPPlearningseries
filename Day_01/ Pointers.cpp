3ae86389de836b723a36316760728b8fd668ae5c
Kushal-np <wrckushal@gmail.com>
2025-07-22 16:18:59 +0545

    pointers begun

---
+++ Day_01/ Pointers.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+#include <vector>
+
+// string food = "Kushal";
+// cout << food;
+// cout << &food;
+
+// string food = "pizza";
+// string* ptr = &food ;
+
+int main(){
+
+    string food = "Pizza";
+    string* ptr = &food;
+
+    cout << food << endl;
+    cout << ptr << endl;
+    cout << &food << endl;
+
+    return 0 ; 
+}
