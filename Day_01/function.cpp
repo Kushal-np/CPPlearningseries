19be16494f52be97213a440cc8e7f7e0bc18cd5e
Kushal-np <wrckushal@gmail.com>
2025-07-22 16:44:51 +0545

    function introductory

---
+++ Day_01/function.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+#include <vector>
+void myFunction();
+
+
+int main(){
+    
+    myFunction();
+    return 0 ; 
+}
+void myFunction(){
+    cout<< "hello world" << endl;
+}
